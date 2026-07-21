a09393e791b017340b86d2e10402c3a74a076676
Sreehari <sreehriz2005@gmail.com>
2026-07-21 23:20:07 +0530

    update 3

--- backend/main.py
+++ backend/main.py
@@ -9,3 +9,4 @@ from app.main import asgi_app as app
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+#Ok what do you want me to do now?
